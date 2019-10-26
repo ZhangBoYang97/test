b2b150f55f445b78673e7220dd11666f78cfa86c
ZhangBoYang97 <50429948+ZhangBoYang97@users.noreply.github.com>
2019-10-26 20:34:01 +0800

    修改练习的java文件.java
    用来练习修改功能的文件

---
+++ Number.java
@@ -0,0 +1,11 @@
+public class ExceptionCauseStudy {
+    public static void main(String[] args) {
+     //这里使用的异常非常的简单，我们用的是数组越界异常
+        int a[] = {1,2,3,4,5};
+        int element = getElement(a, 5);
+        System.out.println(element);
+    }
+    public static int getElement(int arr[],int a){
+        int number = arr[a];
+        return number;
+    }
