35eda0ae15a4c28064d7076a91a1f5dc72bddf9a
ZhangBoYang97 <50429948+ZhangBoYang97@users.noreply.github.com>
2019-10-26 20:39:39 +0800

    bug提交修改
    我发现了一个代码的错误，并提出代码优化的解决方法

--- Number.java
+++ Number.java
@@ -2,7 +2,9 @@ public class ExceptionCauseStudy {
     public static void main(String[] args) {
      //这里使用的异常非常的简单，我们用的是数组越界异常
         int a[] = {1,2,3,4,5};
-        int element = getElement(a, 5);
+      //  int element = getElement(a, 5);
+        // 优化1 访问正确的数值索引下标
+        int element = getElement(a.3);
         System.out.println(element);
     }
     public static int getElement(int arr[],int a){
